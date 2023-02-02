fdfd4767ffb35b3e444aefa66a85a51150d1e612
bishal2408 <bishallamichhane24@gmail.com>
2023-02-02 06:45:18 +0545

    minor changes

--- app/Http/Controllers/Merchant/UserDemographicsController.php
+++ app/Http/Controllers/Merchant/UserDemographicsController.php
@@ -3,12 +3,42 @@
 namespace App\Http\Controllers\Merchant;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserDemographicsController extends Controller
 {
     public function index()
     {
-        return view('merchant.demographics.index');
+        $now = Carbon::now();
+        $startOfWeek = Carbon::now()->startOfWeek();
+        $endOfWeek = Carbon::now()->endOfWeek();
+
+        $products = Product::whereBetween('updated_at', [$startOfWeek, $endOfWeek])->where('merchant_id', Auth::id())->get();
+        $orders = Order::whereBetween('updated_at', [$startOfWeek, $endOfWeek])->get();
+        $count = 0;
+        $total_sales_count = 0;
+        $total_revenue = 0;
+        foreach($products as $product)
+        {
+            if($product->purchase_count > $count ){
+                $count = $product->purchase_count;
+                $data['product_of_the_week'] = $product->name;
+                $data['product_of_the_week_unit_sold'] = Order::where(['product_id', $product->id])->pluck('quantity');
+            }
+            $total_sales_count += $product->purchase_count;
+            foreach($orders as $order){
+                if($order->product_id == $product->id){
+                    $total_revenue += $order->quantity * $product->price;
+                }
+            }
+        }
+        $data['total_sales_this_week'] = $total_sales_count;
+        $data['total_revene_this_week'] = $total_revenue;
+        return $data;
+        return view('merchant.demographics.index', compact('data'));
     }
 }
--- resources/views/merchant/demographics/index.blade.php
+++ resources/views/merchant/demographics/index.blade.php
@@ -27,7 +27,41 @@
                             <h6 class="m-0 font-weight-bold text-primary text-lg" style="float: left !important">User Demographics</h6>
                         </div>
                         <div class="card-body m-1">
-                            
+                            <div class="d-flex justify-content-between ">
+                                <div class="card shadow bg-primary text-white m-2">
+                                    <div class="card-header py-2 flex align-content-center">
+                                        <h6 class="m-0 font-weight-bold text-md" style="float: left !important">Popular Product (this week)</h6> <br>
+                                    </div>
+                                    <div class="card-body m-1">
+                                        <h4 class="text-center" style="font-size: 1rem;">{{ $data['product_of_the_week'] }}</h4>
+                                        <p></p>
+                                    </div>
+                                </div>
+                                <div class="card shadow bg-primary text-white m-2">
+                                    <div class="card-header py-2 flex align-content-center">
+                                        <h6 class="m-0 font-weight-bold text-md" style="float: left !important">Total Revenue (this week)</h6> <br>
+                                    </div>
+                                    <div class="card-body m-1">
+                                        <h4 class="text-center text-md" style="font-size: 1rem;">Rs. {{ $data['total_revene_this_week'] }}</h4>
+                                    </div>
+                                </div>
+                                <div class="card shadow bg-primary text-white m-2">
+                                    <div class="card-header py-2 flex align-content-center">
+                                        <h6 class="m-0 font-weight-bold text-md" style="float: left !important">Total Sales (this week)</h6> <br>
+                                    </div>
+                                    <div class="card-body m-1">
+                                        <h4 class="text-center text-md" style="font-size: 1rem;">{{ $data['total_sales_this_week'] }}</h4>
+                                    </div>
+                                </div>
+                                <div class="card shadow bg-primary text-white m-2">
+                                    <div class="card-header py-2 flex align-content-center">
+                                        <h6 class="m-0 font-weight-bold text-md" style="float: left !important">Age Demographics (this week)</h6> <br>
+                                    </div>
+                                    <div class="card-body m-1">
+                                        <h4 class="text-center text-md" style="font-size: 1rem;">25-30 years old</h4>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="float-right">
                             </div>
                         </div>
